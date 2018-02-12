7e1fb8c16ae981311a92abe6432ef5d623c8277d
IoTDomoticSolutions <domotic.solutions.iot@gmail.com>
2018-02-12 15:58:22 +0100

    Proyecto para presentar 12/02/2018

--- app/src/main/java/com/app1t1617/iotgroup/swithome/Main/MainInitialActivity.java
+++ app/src/main/java/com/app1t1617/iotgroup/swithome/Main/MainInitialActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,23 +25,24 @@ import android.widget.Toast;
 
 import com.app1t1617.iotgroup.swithome.FastActions.FastActionsFragment;
 import com.app1t1617.iotgroup.swithome.Login.MainActivity;
+import com.app1t1617.iotgroup.swithome.Profile.MyProfileFragment;
 import com.app1t1617.iotgroup.swithome.R;
 
 public class MainInitialActivity extends AppCompatActivity {
 
     ListView menu;
-    android.support.v4.app.FragmentManager fragmentManager;
-    android.support.v4.app.FragmentTransaction fragmentTransaction;
-    android.support.v4.app.Fragment fragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    Fragment fragment;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle drawerToggle;
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
+    Toolbar mainToolbar;
 
     View layout;
     Toast toast;
     TextView textToast;
-    Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,12 +88,11 @@ public class MainInitialActivity extends AppCompatActivity {
     }
     private void abrirPantalla(int i){
 
-        fragmentTransaction = fragmentManager.beginTransaction();
+
 
         switch (i) {
             case 0:  // Abrir fragment Inicio
                 clearBackStack();
-                fragment = new MainMenuFragment();
                 break;
             case 1:
                 motherOfToast("Vista en construcción");
@@ -100,33 +103,42 @@ public class MainInitialActivity extends AppCompatActivity {
             case 3:
                 if (mainToolbar.getTitle() != "Funciones rápidas") {
                     clearBackStack();
-                    fragment = new FastActionsFragment();
-                    fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.enter_from_right, R.anim.exit_to_left);
-                    fragmentTransaction.addToBackStack(null);
+                    changeFragment(new FastActionsFragment(), true);
                 }
                 break;
             case 4:
-                motherOfToast("Vista en construcción");
+                if(mainToolbar.getTitle() != "Mi perfil"){
+                    clearBackStack();
+                    changeFragment(new MyProfileFragment(), false);
+                }
                 break;
             case 5:
                 backToLogin();
                 break;
         }
 
+    }
 
+    public void changeFragment(Fragment fragment, Boolean enterLeft){
+        fragmentTransaction = fragmentManager.beginTransaction();
+        if(enterLeft){
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.enter_from_right, R.anim.exit_to_left);
+        }else{
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.enter_from_left, R.anim.exit_to_right);
+        }
 
         fragmentTransaction.replace(R.id.main_frame_layout, fragment, "Fragment");
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
     }
 
     public void clearBackStack(){
         fragmentManager.popBackStack();
-        //fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.main_frame_layout, new MainMenuFragment(), "Fragment");
-        //fragmentTransaction.commit();
+        fragmentTransaction.commit();
     }
-    //Vuelta al login y se eliminan todas las credenciales y los datos del usuario
+
     public void backToLogin(){
         Intent intent = new Intent(this, MainActivity.class);
         editor.remove("token");
--- app/src/main/java/com/app1t1617/iotgroup/swithome/Profile/MyProfileFragment.java
+++ app/src/main/java/com/app1t1617/iotgroup/swithome/Profile/MyProfileFragment.java
@@ -1,12 +1,15 @@
 package com.app1t1617.iotgroup.swithome.Profile;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.app1t1617.iotgroup.swithome.R;
 
@@ -17,6 +20,12 @@ import com.app1t1617.iotgroup.swithome.R;
 public class MyProfileFragment extends Fragment {
     View view;
     Context context;
+
+    TextView username;
+
+    //Declaración de preferencias
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
 
     public MyProfileFragment(){
 
@@ -32,6 +41,18 @@ public class MyProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_provisional_profile, container, false);
+        getActivity().setTitle("Mi perfil");
+
+        username = (TextView) view.findViewById(R.id.profile_username);
+
+        //Obtener de preferencias el dato guardado
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        editor = prefs.edit();
+
+        String name = prefs.getString("nameLogged", "No name defined");
+
+        username.setText(name);
+
 
         return view;
     }
